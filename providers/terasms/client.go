3fc561c8e167815c8de098cf3a8e1a0130a0c1ec
Kihamo <dev@kihamo.ru>
2017-11-27 23:34:48 +0300

    Added sing for terasms provider

--- providers/terasms/client.go
+++ providers/terasms/client.go
@@ -2,10 +2,14 @@ package terasms
 
 import (
 	"context"
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -14,7 +18,7 @@ const (
 	AuthByToken = iota + 1
 	AuthByLoginAndPassword
 
-	ParamSignature = "sig"
+	ParamSignature = "sign"
 	ParamLogin     = "login"
 	ParamPassword  = "password"
 )
@@ -55,7 +59,6 @@ func (c *client) Send(ctx context.Context, phone, message string) error {
 	}
 
 	q := req.URL.Query()
-	q.Set(ParamLogin, c.login)
 	q.Set("target", phone)
 	q.Set("sender", c.sender)
 	q.Set("message", message)
@@ -63,12 +66,14 @@ func (c *client) Send(ctx context.Context, phone, message string) error {
 	req.URL.RawQuery = q.Encode()
 	req.WithContext(ctx)
 
-	resp, err := c.do(req)
+	_, code, err := c.do(req)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(resp, err)
+	if code < 0 {
+		return fmt.Errorf("Provider returns error with code #%d", code)
+	}
 
 	return nil
 }
@@ -85,41 +90,61 @@ func (c *client) Balance(ctx context.Context) (float64, error) {
 
 	req.WithContext(ctx)
 
-	resp, err := c.do(req)
+	resp, code, err := c.do(req)
 	if err != nil {
 		return -1, err
+	}
+
+	if code < 0 {
+		return -1, fmt.Errorf("Provider returns error with code #%d", code)
 	}
 
 	return strconv.ParseFloat(resp, 64)
 }
 
-func (c *client) do(r *http.Request) (string, error) {
+func (c *client) do(r *http.Request) (string, int64, error) {
 	c.sign(r)
 
 	resp, err := http.DefaultClient.Do(r)
 	if err != nil {
-		return "", err
+		return "", 0, err
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return "", 0, err
 	}
 
-	return string(body[:]), nil
+	response := string(body[:])
+	code, _ := strconv.ParseInt(response, 10, 0)
+
+	return response, code, nil
 }
 
 func (c *client) sign(req *http.Request) {
 	q := req.URL.Query()
+	q.Set(ParamLogin, c.login)
 
 	if c.auth == AuthByLoginAndPassword {
-		q.Set(ParamLogin, c.login)
 		q.Set(ParamPassword, c.password)
 	} else {
-		sig := ""
+		params := make([]string, 0, len(q))
+		for k, v := range q {
+			if k == ParamSignature {
+				continue
+			}
 
-		q.Set(ParamSignature, sig)
+			params = append(params, k+"="+v[0])
+		}
+
+		sort.Strings(params)
+
+		sig := md5.New()
+		io.WriteString(sig, strings.Join(params, ""))
+		io.WriteString(sig, c.token)
+
+		q.Set(ParamSignature, hex.EncodeToString(sig.Sum(nil)))
 	}
 
 	req.URL.RawQuery = q.Encode()
